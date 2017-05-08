de959bf47b61afe1a5a1b1af750fd6209f819b60
IrynaRepinetska <repineti@hu-berlin.de>
2017-05-08 18:16:31 +0200

    Create momente.m

---
+++ Praktikum/Blatt2/momente.m
@@ -0,0 +1,26 @@
+function [m1,m2,z2,z3,z4] = momente(daten)
+%prüfe ob daten keine komplexe Zahlen enthalten
+if (isreal(daten)==0)
+    error('an input should not contain complex numbers or strings')
+end
+
+% Berechnung m1
+m1 = summe(daten);
+
+%Berechnung m2
+dummy = daten.*daten;
+m2 = summe(dummy);
+
+%Berechnung z2
+dummy = daten - m1;
+dummy_z2 = power(dummy,2);
+z2 = summe(dummy_z2);
+
+%Berechnung z3
+dummy_z3 = power(dummy,3);
+z3 = summe(dummy_z3);
+
+%Berechnung z4
+dummy_z4 = power(dummy,4);
+z4 = summe(dummy_z4);
+end
